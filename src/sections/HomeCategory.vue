08e67c536fa3474656591671693c5bf1dcc1e923
Onder <onder77@gmail.com>
2019-08-21 21:54:49 +0300

    home sections added

--- src/sections/HomeCategory.vue
+++ src/sections/HomeCategory.vue
@@ -1,10 +1,109 @@
 <template>
- 
+ <!-- ========================= SECTION CONTENT ========================= -->
+<section class="section-content padding-y-sm bg">
+<div class="container">
+
+<header class="section-heading heading-line">
+	<h4 class="title-section bg">MACHINERY</h4>
+</header>
+
+<div class="card">
+<div class="row no-gutters">
+	<div class="col-md-3">
+	
+<article href="#" class="card-banner h-100 bg2">
+	<div class="card-body zoom-wrap">
+		<h5 class="title">Machinery items for manufacturers</h5>
+		<p>Consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, cLorem ipsum dolor sit amet, cLorem ipsum dolor sit amet, cLorem ipsum dolor sit amet, c</p>
+		<a href="#" class="btn btn-warning">Explore</a>
+		<img src="images/items/item-sm.png" height="200" class="img-bg zoom-in">
+	</div>
+</article>
+
+	</div> <!-- col.// -->
+	<div class="col-md-9">
+<ul class="row no-gutters border-cols">
+	<li class="col-6 col-md-3">
+<a href="#" class="itembox"> 
+	<div class="card-body">
+		<p class="word-limit">Home and kitchen electronic  stuff collection  </p>
+		<img class="img-sm" src="images/items/1.jpg">
+	</div>
+</a>
+	</li>
+	<li class="col-6 col-md-3">
+<a href="#" class="itembox"> 
+	<div class="card-body">
+		<p class="word-limit">Kitchen equipments collection</p>
+		<img class="img-sm" src="images/items/2.jpg">
+	</div>
+</a>
+	</li>
+	<li class="col-6 col-md-3">
+<a href="#" class="itembox"> 
+	<div class="card-body">
+		<p class="word-limit">Accessiries and other good items</p>
+		<img class="img-sm" src="images/items/3.jpg">
+	</div>
+</a>
+	</li>
+	<li class="col-6 col-md-3">
+<a href="#" class="itembox"> 
+	<div class="card-body">
+		<p class="word-limit">Techs, Electronics</p>
+		<img class="img-sm" src="images/items/4.jpg">
+	</div>
+</a>	
+	</li>
+</ul>
+<ul class="row no-gutters border-cols">
+	<li class="col-6 col-md-3">
+<a href="#" class="itembox"> 
+	<div class="card-body">
+		<p class="word-limit">Home and kitchen electronic stuff </p>
+		<img class="img-sm" src="images/items/1.jpg">
+	</div>
+</a>
+	</li>
+	<li class="col-6 col-md-3">
+<a href="#" class="itembox"> 
+	<div class="card-body">
+		<p class="word-limit">Kitchen equipments collection</p>
+		<img class="img-sm" src="images/items/2.jpg">
+	</div>
+</a>
+	</li>
+	<li class="col-6 col-md-3">
+<a href="#" class="itembox"> 
+	<div class="card-body">
+		<p class="word-limit">Accessiries and other good items</p>
+		<img class="img-sm" src="images/items/3.jpg">
+	</div>
+</a>
+	</li>
+	<li class="col-6 col-md-3">
+<a href="#" class="itembox"> 
+	<div class="card-body">
+		<p class="word-limit">Techs, Electronics</p>
+		<img class="img-sm" src="images/items/4.jpg">
+	</div>
+</a>	
+	</li>
+</ul>
+	</div> <!-- col.// -->
+</div> <!-- row.// -->
+	
+</div> <!-- card.// -->
+
+</div> <!-- container .//  -->
+</section>
+<!-- ========================= SECTION CONTENT END// ========================= -->
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "HomeCategory",
   props: {
     msg: String
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,13 @@
   <div>
     <Navbar></Navbar>
     <HomeSlider></HomeSlider>
-
-  
-
+    <HomeCategory></HomeCategory>
+    <HomeCategory></HomeCategory>
+    <HomeRequestForQuotation></HomeRequestForQuotation>
+    <HomeRecommendedItems></HomeRecommendedItems>
+    <HomeLinks></HomeLinks>
+    <HomeSubscribe></HomeSubscribe>
+<Footer></Footer>
   </div>
 </template>
 
@@ -12,11 +16,21 @@
 // @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
 import HomeSlider from "@/sections/HomeSlider.vue";
+import HomeCategory from "@/sections/HomeCategory.vue";
+import HomeRequestForQuotation from "@/sections/HomeRequestForQuotation.vue";
+import HomeRecommendedItems from "@/sections/HomeRecommendedItems.vue";
+import HomeLinks from "@/sections/HomeLinks.vue";
+import HomeSubscribe from "@/sections/HomeSubscribe.vue";
+import Footer from "@/sections/Footer.vue";
+
+
+
+
 
 export default {
   name: "home",
   components: {
-    Navbar, HomeSlider
+    Navbar, HomeSlider, HomeCategory, HomeRequestForQuotation, HomeRecommendedItems,HomeLinks, HomeSubscribe, Footer
   }
 };
 </script>
